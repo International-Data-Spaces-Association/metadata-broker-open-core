6bbab620fbec8b9468af98991171b8236fa6209f
Matthias Böckmann <matthias.boeckmann@iais.fraunhofer.de>
2021-03-09 10:37:02 +0100

    For compatibility reasons, re-adding variable with @Deprecated tag

--- open-index-common/src/main/java/de/fraunhofer/iais/eis/ids/index/common/main/MainTemplate.java
+++ open-index-common/src/main/java/de/fraunhofer/iais/eis/ids/index/common/main/MainTemplate.java
@@ -33,7 +33,9 @@ public abstract class MainTemplate implements ComponentInteractorProvider {
     public String elasticsearchHostname;
     public int elasticsearchPort;
     public String keystorePassword, keystoreAlias;
-//    public String componentIdsId;
+
+    @Deprecated
+    public String componentIdsId;
     public String dapsUrl;
     public boolean trustAllCerts, ignoreHostName;
 

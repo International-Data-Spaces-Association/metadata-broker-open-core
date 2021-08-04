96253aefdbb480565f44353394b2ca6384a6566d
Matthias Böckmann <matthias.boeckmann@iais.fraunhofer.de>
2021-08-04 14:33:10 +0200

    Repairing recreation of index Turning Indexing into generic Interface

--- open-index-common/src/main/java/de/fraunhofer/iais/eis/ids/index/common/persistence/NullIndexing.java
+++ open-index-common/src/main/java/de/fraunhofer/iais/eis/ids/index/common/persistence/NullIndexing.java
@@ -1,25 +1,26 @@
 package de.fraunhofer.iais.eis.ids.index.common.persistence;
 
 
-import de.fraunhofer.iais.eis.InfrastructureComponent;
-import de.fraunhofer.iais.eis.Participant;
 import de.fraunhofer.iais.eis.ids.index.common.persistence.spi.Indexing;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.URI;
 
-public class NullIndexing implements Indexing {
+/**
+ * Dummy indexing that can accept any kind of class from the IDS information model and ignores the indexing
+ */
+public class NullIndexing<T> implements Indexing<T> {
 
     final private Logger logger = LoggerFactory.getLogger(NullIndexing.class);
 
     @Override
-    public void add(InfrastructureComponent selfDescription) {
+    public void add(T selfDescription) {
         logger.info("Add to index IGNORED.");
     }
 
     @Override
-    public void update(InfrastructureComponent selfDescription) {
+    public void update(T selfDescription) {
         logger.info("Index update IGNORED.");
     }
 
@@ -28,21 +29,6 @@ public class NullIndexing implements Indexing {
         logger.info("Delete from index IGNORED.");
     }
 
-    @Override
-    public void add(Participant participant) {
-        logger.info("Add to index IGNORED.");
-    }
-
-    @Override
-    public void update(Participant participant) {
-        logger.info("Index update IGNORED.");
-    }
-
-    @Override
-    @Deprecated
-    public void delete(Participant participant) {
-        logger.info("Delete from index IGNORED.");
-    }
 
     @Override
     public void recreateIndex(String indexName) {
